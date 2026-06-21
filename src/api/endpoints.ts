df3135fa2c77787da7f76cf9682f196aa16d73c2
Hana Amelia <Luminovoez@outlook.com>
2026-06-21 22:57:38 +0800

    Update endpoints.ts

--- src/api/endpoints.ts
+++ src/api/endpoints.ts
@@ -5,4 +5,4 @@ export const ENDPOINT = "https://x.moonchan.xyz/api/twitter"
 // export const ENDPOINT = "/api/twitter"
 
 export const DEFAULT_IMAGE_PROXY = "https://twimg.moonchan.xyz";
-export const DEFAULT_VIDEO_PROXY = "https://video-cf.twimg.com";
+export const DEFAULT_VIDEO_PROXY = "https://twimg.moonchan.xyz";
